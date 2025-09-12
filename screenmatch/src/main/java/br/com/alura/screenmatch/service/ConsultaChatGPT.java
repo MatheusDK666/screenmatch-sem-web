447f153e85f7d06785958efb000f2d7c447fa73c
MatheusDK666 <kevinmatheus1666@gmail.com>
2025-09-12 18:05:49 -0300

    Quarto comit

--- screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("KEY SECRET");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
